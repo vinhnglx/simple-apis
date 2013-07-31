45fac448aa7acf8d22a58373b9c5375a6aa8558c
Vinh Nguyen Le <vinh.nguyenlexuan@asnet.com.vn>
2013-07-31 20:59:41 +0700

    initialize code base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleApis::Application.config.secret_key_base = 'e703983ea7363e6ca2c434d05db2bbd63bee1a038ee24cd485ceb5d9c8426e21a4d160a6713117fe87609183d297203f783fa9befc746c7c23b0f985cb02fa10'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleApis::Application.config.session_store :cookie_store, key: '_simple-apis_session'
