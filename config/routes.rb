0166020992be0358d69c2f7e396e51a070065444
Vinh Nguyen Le <vinh.nguyenlexuan@asnet.com.vn>
2013-07-31 21:18:08 +0700

    set association for product and category

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SimpleApis::Application.routes.draw do
+  resources :categories
+
+  resources :products
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
